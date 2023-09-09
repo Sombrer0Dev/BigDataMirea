614fc503a8daddfed4bd1d0ade3068af16185a50
Sokolov Artem <smbrer1@gmail.com>
2023-09-10 01:33:06 +0300

    finished task1

--- main.py
+++
@@ -1,9 +0,0 @@
-from sklearn.datasets import fetch_california_housing
-import pandas as pd
-from pandas import DataFrame
-
-if __name__ == '__main__':
-    data = fetch_california_housing(as_frame=True)
-    data: DataFrame = pd.DataFrame(data.data,columns=data.feature_names)
-
-    data.isna()
